63596e4162e64dda71475f69294b45d1109d5e21
David <121581851+DCall4@users.noreply.github.com>
2023-07-11 17:24:54 -0700

    Worked on Page routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home';
+import Shop from './Pages/Shop';
+import Checkout from './Pages/Checkout';
+import Products from './Pages/Products';
+import NotFound from './Pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/shop">Shop</Link></li>
+        <li><Link to="/checkout">Checkout</Link></li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/shop" element={<Shop/>} />
+      <Route path='/shop/:id' element={<Products/>}/>
+      <Route path='/checkout' element={<Checkout/>} />
+      <Route path='*' element={<NotFound/>} />
+    </Routes>
+    </>
   );
 }
 
